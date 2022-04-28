a2777fcdc7243c7a79d9b1d551c9b8d64b5eca55
Emir Maestu <emir13_@hotmail.com>
2022-04-28 01:54:41 -0300

    add context

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import SearchResults from './pages/SearchResults'
 import Detail from './pages/Detail'
 
 import { Link, Route } from 'wouter'
+import StaticContext from './context/StaticContext'
 
 function App() {
     return (
-        <div className='App'>
-            <section className='App-content'>
-                <Link to='/'>
-                    <img
-                        className='App-logo'
-                        alt='Giffy logo'
-                        src='/logo.png'
-                    />
-                </Link>
-                <Route component={Home} path='/' />
-                <Route component={SearchResults} path='/search/:keyword' />
-                <Route component={Detail} path='/gif/:id' />
-            </section>
-        </div>
+        <StaticContext.Provider value={{ name: 'Emir' }}>
+            <div className='App'>
+                <section className='App-content'>
+                    <Link to='/'>
+                        <img
+                            className='App-logo'
+                            alt='Giffy logo'
+                            src='/logo.png'
+                        />
+                    </Link>
+                    <Route component={Home} path='/' />
+                    <Route component={SearchResults} path='/search/:keyword' />
+                    <Route component={Detail} path='/gif/:id' />
+                </section>
+            </div>
+        </StaticContext.Provider>
     )
 }
 
---
+++ src/context/StaticContext.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Context = React.createContext({
+    name: 'test',
+})
+export default Context
